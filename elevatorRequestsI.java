e0af22289de1dcec048b28c402232b0a4f635326
kmralok795 <kr7908alok@gmail.com>
2026-08-15 21:47:49 +0530

    Implement elevator request time calculation

---
+++ elevatorRequestsI.java
@@ -0,0 +1,18 @@
+class Main {
+    public static int elevatorRequests(int n, int[] requests) {
+
+        int currentFloor = 0;
+        int totalTime = 0;
+
+        for (int request : requests) {
+            totalTime += Math.abs(currentFloor - request);
+            currentFloor = request;
+        }
+
+        return totalTime;
+    }
+    
+    public static void main(String ar[]){
+        int[] requests={2,1,4,3};
+        System.out.println("Total Time taken to complete the requests: "+elevatorRequests(5, requests));
+    }
